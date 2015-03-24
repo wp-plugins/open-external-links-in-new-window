be81843ecf6fb2b67b73b1b68d40ff55dd1ba7c6
Keef <Keef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-24 18:10:59 +0000

    Path correction
    git-svn-id: https://plugins.svn.wordpress.org/open-external-links-in-new-window/trunk@1119893 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- oelinw_options.php
+++ oelinw_options.php
@@ -1,7 +1,7 @@
 <div id="wpbody-content">
 <h1>Open External Links In New Windows</h1>
 <div>
-<p>If you've found this plugin useful and want to say thanks, then a link on your site or a good rating in the <a href="http://wordpress.org/extend/plugins/open-external-links-in-new-windows/">WordPress Plugin Directory</a> will be more than appreciated.</p>
+<p>If you've found this plugin useful and want to say thanks, then a link on your site or a good rating in the <a href="http://wordpress.org/extend/plugins/open-external-links-in-new-window/">WordPress Plugin Directory</a> will be more than appreciated.</p>
 <p>If you'd like to make a small Paypal donation to support future developments, that would be awesome and most gratefully received.</p>
 <div id="donate">
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
